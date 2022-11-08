5ff7adb81d7136232390c7fd27ac6b4637709791
Daniel Nilsson <daniel.nilsson1@iths.se>
2022-11-08 11:58:25 +0100

    finished L5.1 dashboard

--- Code-alongs/L5.1-stock_dashboard/time_filtering.py
+++ Code-alongs/L5.1-stock_dashboard/time_filtering.py
@@ -3,6 +3,6 @@ from dateutil.relativedelta import relativedelta
 
 def filter_time(df, days = 0):
     last_day = df.index[0].date()
-    start_day = last_day-relativedelta(days=days)
+    start_day = last_day-relativedelta(days = days)
     df = df.sort_index().loc[start_day:last_day]
     return df
